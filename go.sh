359947808ea3160bd70401e4a8bc43b3bd1772b8
leyiyi716 <18409484300@163.com>
2016-04-19 20:41:26 +0800

    dd

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
-git comfit --global user.email "18409484300@163.com"
-git comfit --global user.name "leyiyi716"
+git comfig --global user.email "18409484300@163.com"
+git comfig --global user.name "leyiyi716"
 git add .
-git comit -m$1
+git commit -m $1
 git push
